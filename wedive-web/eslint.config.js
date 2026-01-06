c3bf87171ca6ff9608279050ac23866425dbc383
cloud-solutions-ttakeuchi <t.takeuchi.a@gmail.com>
2026-01-06 20:27:11 +0900

    chore: ignore sqlite worker scripts in eslint

--- wedive-web/eslint.config.js
+++ wedive-web/eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'public']),
+  globalIgnores(['dist', 'public', 'src/assets/jswasm']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
